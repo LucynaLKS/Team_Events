fcb2a078abd7905653d243fa3b93b02b30500430
lucynka <Lks@hoga.pl>
2021-09-13 13:54:15 +0200

    feat: improvement of tests

--- cypress/integration/tests_e2e/responsiveWebsite.spec.js
+++ cypress/integration/tests_e2e/responsiveWebsite.spec.js
@@ -6,33 +6,63 @@ describe("responsiveness website", () => {
     cy.visit("/");
   });
 
-  it("checking the style values after changing the viewport to a mobile look - firt item", () => {
+  it("checking the image style values after changing the viewport to a mobile look - firt item", () => {
+    cy.viewport(320, 568).wait(2000)
+    cy.get('.place-card .images')
+    .invoke('outerWidth').should('be.lt', 350);
+  });
+
+  it("checking the display view after changing the viewport to a mobile look - firt item", () => {
     cy.viewport(320, 568).wait(2000)
     cy.get('.place-card')
-    .should('have.css', 'width', '258px')
+    .should('have.css', 'display', 'block');
   });
 
-  it("checking the style values after changing the viewport to a mobile look - second item", () => {
+  it("checking the image style values after changing the viewport to a mobile look - second item", () => {
+    cy.viewport(360, 640).wait(2000)
+    cy.get('.place-card .images')
+    .invoke('outerWidth').should('be.lt', 350);
+  });
+
+  it("checking the display view after changing the viewport to a mobile look - second item", () => {
     cy.viewport(360, 640).wait(2000)
     cy.get('.place-card')
-    .should('have.css', 'width', '298px')
+    .should('have.css', 'display', 'block');
   });
 
-  it("checking the style values after changing the viewport to a tablet look", () => {
+  it("checking the image style values after changing the viewport to a tablet look", () => {
+    cy.viewport(1024, 1366)
+    cy.get('.place-card .images')
+    .invoke('outerWidth').should('be.lt', 351);
+  });
+
+  it("checking the display view after changing the viewport to a tablet look", () => {
     cy.viewport(1024, 1366)
     cy.get('.place-card')
-    .should('have.css', 'width', '994px')
+    .should('have.css', 'display', 'flex');
   });
 
-  it("checking the style values after changing the viewport to a desktop look", () => {
+  it("checking the image style values after changing the viewport to a desktop look", () => {
+    cy.viewport(1366, 768)
+    cy.get('.place-card .images')
+    .invoke('outerWidth').should('be.lt', 351);
+  });
+
+  it("checking the display view after changing the viewport to a desktop look", () => {
     cy.viewport(1366, 768)
     cy.get('.place-card')
-    .should('have.css', 'width', '1100px')
+    .should('have.css', 'display', 'flex');
   });
 
-  it("checking the style values after changing the viewport to a desktop look (Full HD)", () => {
+  it("checking the image style values after changing the viewport to a desktop look (Full HD)", () => {
+    cy.viewport(1920, 1080)
+    cy.get('.place-card .images')
+    .invoke('outerWidth').should('be.lt', 351);
+  });
+
+  it("checking the display view after changing the viewport to a desktop look (Full HD)", () => {
     cy.viewport(1920, 1080)
     cy.get('.place-card')
-    .should('have.css', 'width', '1100px')
+    .should('have.css', 'display', 'flex');
   });
 });
