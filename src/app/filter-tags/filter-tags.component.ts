2d38d5f6ebea791f8c92092a5e097eb830e3049f
rpietrek <rafal.pietrek@hyland.com>
2021-09-28 13:58:21 +0200

    wip

---
+++ src/app/filter-tags/filter-tags.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-tags',
+  templateUrl: './filter-tags.component.html',
+  styleUrls: ['./filter-tags.component.scss'],
+})
+export class FilterTagsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
